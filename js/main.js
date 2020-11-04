caf6bed401686bc60c9d0c76d1d2ba70480d6d3b
Andrei Dzebysh <presnia@yahoo.com>
2020-11-04 11:22:44 +0300

    The form works

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-let personData = {
-  
-}
+let fName = document.querySelector('.fname'),
+    lName = document.querySelector('.lname'),
+    email = document.querySelector('.email'),
+    birthDay = document.querySelector('.date'),
+    message = document.querySelector('.message'),
+    submit = document.querySelector('.submit');
+
+submit.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    let personData = {
+      fname: fName.value,
+      lname: lName.value,
+      email: email.value,
+      bday: birthDay.value,
+      message: message.value,
+    };
+    
+    console.log(personData);
+});    
+
